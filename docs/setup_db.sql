dd5c4098263a451ce7f9a9fc9ecf00259d8dcea2
rinjugatla <rin.jugatla@gmail.com>
2022-05-18 12:33:11 +0900

    SQLを更新

--- docs/setup_db.sql
+++ docs/setup_db.sql
@@ -1,107 +1,163 @@
--- ---
--- Globals
--- ---
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: 127.0.0.1
+-- 生成日時: 2022-05-18 05:32:35
+-- サーバのバージョン： 10.7.3-MariaDB
+-- PHP のバージョン: 8.1.1
 
--- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
--- SET FOREIGN_KEY_CHECKS=0;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
--- ---
--- Table 'tts_files'
--- 
--- ---
 
-DROP TABLE IF EXISTS `tts_files`;
-		
-CREATE TABLE `tts_files` (
-  `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `text` VARCHAR(4000) NOT NULL,
-  `hash` VARCHAR(100) NOT NULL,
-  `extension` VARCHAR(4) NOT NULL,
-  `created_at` DATETIME NOT NULL DEFAULT current_timestamp(),
-  `updated_at` DATETIME NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`id`)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- ---
--- Table 'tts_words'
--- 
--- ---
+--
+-- データベース: `teuteu_discord_server`
+--
+CREATE DATABASE IF NOT EXISTS `teuteu_discord_server` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `teuteu_discord_server`;
 
-DROP TABLE IF EXISTS `tts_words`;
-		
-CREATE TABLE `tts_words` (
-  `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `pattern` VARCHAR(200) NOT NULL,
-  `replace` VARCHAR(200) NOT NULL,
-  `created_at` DATETIME NOT NULL DEFAULT current_timestamp(),
-  `updated_at` DATETIME NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`id`)
-);
+-- --------------------------------------------------------
 
--- ---
--- Table 'apex_users'
--- 
--- ---
+--
+-- テーブルの構造 `apex_users`
+--
 
-DROP TABLE IF EXISTS `apex_users`;
-		
 CREATE TABLE `apex_users` (
-  `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `name` INTEGER NOT NULL,
-  `uid` INTEGER NOT NULL,
-  `platform` INTEGER NOT NULL,
-  `created_at` DATETIME NOT NULL DEFAULT current_timestamp(),
-  `updated_at` DATETIME NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`id`)
-);
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `uid` bigint(20) NOT NULL,
+  `platform` varchar(4) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- ---
--- Table 'apex_user_rank'
--- 
--- ---
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `apex_user_rank`;
-		
-CREATE TABLE `apex_user_rank` (
-  `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `apex_user_id` INTEGER NOT NULL,
-  `season` INTEGER NOT NULL,
-  `split` INTEGER NOT NULL,
-  `battle_score` INTEGER NOT NULL,
-  `battle_name` VARCHAR(20) NOT NULL,
-  `battle_division` INTEGER NOT NULL,
-  `arena_score` INTEGER NOT NULL,
-  `arena_name` VARCHAR(20) NOT NULL,
-  `arena_division` INTEGER NOT NULL,
-  `created_at` DATETIME NOT NULL DEFAULT current_timestamp(),
-  `updated_at` DATETIME NOT NULL DEFAULT DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`id`)
-);
+--
+-- テーブルの構造 `apex_user_ranks`
+--
 
--- ---
--- Foreign Keys 
--- ---
+CREATE TABLE `apex_user_ranks` (
+  `id` int(11) NOT NULL,
+  `apex_user_id` int(11) NOT NULL,
+  `season` int(11) NOT NULL,
+  `split` int(11) NOT NULL,
+  `battle_score` int(11) NOT NULL,
+  `battle_name` varchar(20) NOT NULL,
+  `battle_division` int(11) NOT NULL,
+  `arena_score` int(11) NOT NULL,
+  `arena_name` varchar(20) NOT NULL,
+  `arena_division` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `apex_user_rank` ADD FOREIGN KEY (apex_user_id) REFERENCES `apex_users` (`id`);
+-- --------------------------------------------------------
 
--- ---
--- Table Properties
--- ---
+--
+-- テーブルの構造 `tts_files`
+--
 
--- ALTER TABLE `tts_files` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `tts_words` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `apex_users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `apex_user_rank` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+CREATE TABLE `tts_files` (
+  `id` int(11) NOT NULL,
+  `text` varchar(4000) NOT NULL,
+  `hash` varchar(200) NOT NULL,
+  `extension` varchar(4) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- ---
--- Test Data
--- ---
+-- --------------------------------------------------------
 
--- INSERT INTO `tts_files` (`id`,`text`,`hash`,`extension`,`created_at`,`updated_at`) VALUES
--- ('','','','','','');
--- INSERT INTO `tts_words` (`id`,`pattern`,`replace`,`created_at`,`updated_at`) VALUES
--- ('','','','','');
--- INSERT INTO `apex_users` (`id`,`name`,`uid`,`platform`,`created_at`,`updated_at`) VALUES
--- ('','','','','','');
--- INSERT INTO `apex_user_rank` (`id`,`apex_user_id`,`season`,`split`,`battle_score`,`battle_name`,`battle_division`,`arena_score`,`arena_name`,`arena_division`,`created_at`,`updated_at`) VALUES
--- ('','','','','','','','','','','','');
+--
+-- テーブルの構造 `tts_words`
+--
+
+CREATE TABLE `tts_words` (
+  `id` int(11) NOT NULL,
+  `pattern` varchar(100) NOT NULL,
+  `new` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `apex_users`
+--
+ALTER TABLE `apex_users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- テーブルのインデックス `apex_user_ranks`
+--
+ALTER TABLE `apex_user_ranks`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idx_01` (`apex_user_id`);
+
+--
+-- テーブルのインデックス `tts_files`
+--
+ALTER TABLE `tts_files`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uq_01` (`hash`);
+
+--
+-- テーブルのインデックス `tts_words`
+--
+ALTER TABLE `tts_words`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uq_01` (`pattern`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `apex_users`
+--
+ALTER TABLE `apex_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- テーブルの AUTO_INCREMENT `apex_user_ranks`
+--
+ALTER TABLE `apex_user_ranks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- テーブルの AUTO_INCREMENT `tts_files`
+--
+ALTER TABLE `tts_files`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- テーブルの AUTO_INCREMENT `tts_words`
+--
+ALTER TABLE `tts_words`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- ダンプしたテーブルの制約
+--
+
+--
+-- テーブルの制約 `apex_user_ranks`
+--
+ALTER TABLE `apex_user_ranks`
+  ADD CONSTRAINT `ref_apex_users_id` FOREIGN KEY (`apex_user_id`) REFERENCES `apex_users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
