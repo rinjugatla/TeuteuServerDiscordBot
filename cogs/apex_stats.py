6ba8b5845ba2a26bfdb2a808bd2dbbbff9ff3fc6
rinjugatla <rin.jugatla@gmail.com>
2022-07-05 01:00:07 +0900

    Apexユーザ情報、ランク情報の登録などを別クラスに分離

--- cogs/apex_stats.py
+++ cogs/apex_stats.py
@@ -7,6 +7,7 @@ from discord.ext.commands import Cog
 from models.bot.apex_user_rank_model import ApexUserRankModel
 from models.database.apex_user_database_model import ApexUserDatabaseModel
 from models.database.apex_user_rank_database_model import ApexUserRankDatabaseModel
+from utilities.apex_user_rank_utility import ApexUserRankUtility
 from utilities.apis.apex_legends_status_api import ApexLegendsStatusAPI
 from utilities.database.database_apex_user import DatabaseApexUserUrility
 from utilities.database.database_apex_user_rank import DatabaseApexUserRankUrility
@@ -22,9 +23,9 @@ class ApexStats(Cog):
     def __init__(self, bot: Client):
         self.bot = bot
         self.is_on_ready_done = False
-        self.is_first_update_user_rank_done = False
         self.updating_user_ranks = False
-        self.prev_update_user_ranks : list[ApexUserRankDatabaseModel] = None
+        self.is_first_update_user_rank_done = False
+        self.rank_utility = ApexUserRankUtility()
         self.post_channel : TextChannel = None
 
     @Cog.listener(name='on_ready')
@@ -47,7 +48,7 @@ class ApexStats(Cog):
 
         users_rank = None
         try:
-            users_rank = await self.refresh_apex_users_rank()
+            users_rank = await self.rank_utility.refresh_apex_users_rank()
         except Exception as ex:
             LogUtility.print_error(str(ex), 'update_user_ranks', traceback.format_exc())
 
@@ -55,7 +56,7 @@ class ApexStats(Cog):
         if users_rank is None:
             return
         
-        changed_users_rank = self.get_changed_user_ranks(users_rank)
+        changed_users_rank = self.rank_utility.get_changed_user_ranks(users_rank)
         if changed_users_rank is None or len(changed_users_rank) == 0:
             return
 
@@ -73,27 +74,6 @@ class ApexStats(Cog):
         for i in range(0, len(embeds), limit):
             await self.post_channel.send(embeds=embeds[i: i+limit])
 
-    def get_changed_user_ranks(self, users_rank: list[ApexUserRankDatabaseModel]) -> list[ApexUserRankDatabaseModel]:
-        """前回と異なるランク情報のみを取得
-
-        Args:
-            users_rank (list[ApexUserRankDatabaseModel]): 今回のユーザランク情報
-
-        Returns:
-            list[ApexUserRankDatabaseModel]: 更新のあったランク情報
-        """
-        if self.prev_update_user_ranks is None:
-            self.store_prev_users_rank(users_rank)
-            return users_rank
-        
-        changed_users_rank = [user for user in users_rank if not user in self.prev_update_user_ranks]
-        self.store_prev_users_rank(users_rank)
-
-        return changed_users_rank
-
-    def store_prev_users_rank(self, users_rank: list[ApexUserRankDatabaseModel]):
-        self.prev_update_user_ranks = users_rank
-
     user_command_group = SlashCommandGroup("apex_user", "ランク情報を追跡するプレイヤの操作")
     rank_command_group = SlashCommandGroup("apex_rank", "ランク情報の操作")
 
@@ -107,7 +87,7 @@ class ApexStats(Cog):
             await context.respond('uidまたはnameを指定してください。')
             return
         try:
-            user = await self.regist_apex_user(uid, name, platform)
+            user = await self.rank_utility.regist_apex_user(uid, name, platform)
         except Exception as ex: 
             await context.respond(str(ex))
             return
@@ -120,7 +100,7 @@ class ApexStats(Cog):
     @user_command_group.command(name='show', description='ランク情報追跡ユーザ一覧を表示')
     async def apex_user_show(self, context: ApplicationContext):
         await context.defer()
-        users = self.get_registerd_users()
+        users = self.rank_utility.get_registerd_users()
         if users is None or len(users) == 0:
             await context.respond(f'ユーザが登録されていません。先に[/apex_user add ~]を実行してください。')
             return
@@ -133,18 +113,14 @@ class ApexStats(Cog):
     async def apex_user_remove(self, context: ApplicationContext,
                                 uid: Option(int, 'UID', required=True)):
         await context.defer()
-        users = self.get_registerd_users()
-        if users is None or len(users) == 0:
-            await context.respond(f'ユーザが登録されていません。先に[/apex_user add ~]を実行してください。')
+        
+        user = None
+        try:
+            user = await self.rank_utility.get_apex_user(uid)
+        except Exception as ex:
+            await context.respond(str(ex))
             return
 
-        regsted_uids = [user.uid for user in users]
-        is_registerd = (uid in regsted_uids)
-        if not is_registerd:
-            await context.respond(f'ユーザ({uid})が登録されていません。')
-            return
-        
-        user = [user for user in users if user.uid == uid][0]
         with DatabaseApexUserUrility() as database:
             database.delete_by_uid(user)
         await context.respond(f'{user.name}({uid})のランク情報追跡を取り消しました。')
@@ -154,23 +130,19 @@ class ApexStats(Cog):
                                  uid: Option(int, 'UID', required=True),
                                  url: Option(str, 'アイコンURL', required=True)):
         await context.defer()
-        users = self.get_registerd_users()
-        if users is None or len(users) == 0:
-            await context.respond(f'ユーザが登録されていません。先に[/apex_user add ~]を実行してください。')
-            return
-
-        regsted_uids = [user.uid for user in users]
-        is_registerd = (uid in regsted_uids)
-        if not is_registerd:
-            await context.respond(f'ユーザ({uid})が登録されていません。')
-            return
 
         is_valid_url = await self.is_valid_image_url(url)
         if not is_valid_url:
             await context.respond(f'画像URLが不正です。JPEGまたはPNGの画像を指定してください。')
             return
 
-        user = [user for user in users if user.uid == uid][0]
+        user = None
+        try:
+            user = await self.rank_utility.get_apex_user(uid)
+        except Exception as ex:
+            await context.respond(str(ex))
+            return
+
         with DatabaseApexUserUrility() as database:
             database.update_icon_url_by_uid(user, url)
             
@@ -195,7 +167,14 @@ class ApexStats(Cog):
                             uid: Option(int, 'uid', required=True),
                             detail: Option(bool, '詳細な情報を表示するか', default=False, required=False)):
         await context.defer()
-        user = await self.refresh_apex_user_rank(uid)
+        
+        try:
+            await self.rank_utility.get_apex_user(uid)
+        except Exception as ex:
+            await context.respond(str(ex))
+            return
+
+        user = await self.rank_utility.refresh_apex_user_rank(uid)
         if user is None:
             await context.respond(f'ユーザ情報({uid})の取得に失敗しました。')
             return
@@ -205,13 +184,13 @@ class ApexStats(Cog):
     async def apex_rank_show_all(self, context: ApplicationContext,
                             detail: Option(bool, '詳細な情報を表示するか', default=False, required=False)):
         await context.defer()
-        users_rank = await self.refresh_apex_users_rank()
+        users_rank = await self.rank_utility.refresh_apex_users_rank()
         embeds = [user.embed for user in users_rank]
         if embeds is None or len(embeds) == 0:
             await context.respond('ユーザが登録されていません。先に[/apex_user add ~]を実行してください。')
             return
         
-        self.store_prev_users_rank(users_rank)
+        self.rank_utility.store_prev_users_rank(users_rank)
         
         limit = 10 # embedは10個まで
         if len(embeds) <= limit:
@@ -224,124 +203,6 @@ class ApexStats(Cog):
     # @rank_command_group.command(name='refresh', description='ランク情報を強制的に更新する')
     # async def apex_rank_refresh(self, context: ApplicationContext):
     #     pass
-
-    async def regist_apex_user(self, uid: int, name: str, platform: str) -> Union[ApexUserRankModel, None]:
-        """ユーザ情報をDBに登録
-
-        Args:
-            uid (int): uidまたはnameの指定が必須
-            name (str): uidまたはnameの指定が必須
-            platform (str): プラットフォーム(PC, PS4, X1, SWITCH)
-
-        Returns:
-            Union[ApexUserRankModel, None]: ランク情報を含むユーザ情報
-        """
-        if uid is None and name is None:
-            return None
-        
-        user = None
-        try:
-            if not uid is None:
-                user = await ApexLegendsStatusAPI.get_user_by_uid(uid, platform)
-            elif not name is None:
-                user = await ApexLegendsStatusAPI.get_user_by_name(name, platform)
-        except:
-            raise
-
-        if user is None:
-            return None
-        with DatabaseApexUserUrility() as database:
-            database.update_by_uid(user)
-            return user
-
-    def get_registerd_users(self) -> list[ApexUserDatabaseModel]:
-        """データベースに登録済みのユーザ情報を取得
-        """
-        with DatabaseApexUserUrility() as database:
-            users_list = database.select_users()
-            users = [ApexUserDatabaseModel(user) for user in users_list]
-            return users
-
-    async def refresh_apex_user_rank(self, uid: int) -> Union[ApexUserRankModel, ApexUserRankDatabaseModel, None]:
-        """データベースに登録済みの追跡対象ユーザのランク情報を取得して登録
-        """
-        user = None
-        with DatabaseApexUserUrility() as database:
-            user: ApexUserDatabaseModel = database.select_by_uid(uid=uid)
-
-        rank_histories = None
-        refreshed_user = await ApexLegendsStatusAPI.get_user_by_uid(user.uid, user.platform)
-        with DatabaseApexUserRankUrility() as database:
-            database.insert_rank_by_uid(refreshed_user)
-            rank_histories = database.select_by_user_uid(user.uid, 2)
-
-        if rank_histories is None:
-            return refreshed_user
-        ranks = self.calc_user_rank_changes(rank_histories)
-        if ranks is None:
-            return None
-        return ranks[-1]
-    
-    async def refresh_apex_users_rank(self) -> Union[list[ApexUserRankModel], list[ApexUserRankDatabaseModel], None]:
-        """データベースに登録されている追跡対象のユーザすべてのランク情報を取得して登録
-        """
-        users = self.get_registerd_users()
-        if users is None or len(users) == 0:
-            return None
-
-        rank_histories = None
-        refreshed_users = [await ApexLegendsStatusAPI.get_user_by_uid(uid=user.uid, platform=user.platform) for user in users]
-        with DatabaseApexUserRankUrility() as database:
-            database.insert_ranks_by_uid(refreshed_users)
-            uids = [user.uid for user in users]
-            rank_histories = database.select_by_users_uid(uids, 2)
-
-        if rank_histories is None:
-            return refreshed_users
-        
-        # 最新のランク情報のみ抽出
-        users_ranks = self.calc_users_ranks_changes(rank_histories)
-        users_rank = [users_rank[-1] for users_rank in users_ranks]
-        return users_rank
-
-    def calc_user_rank_changes(self, ranks: list[dict]) -> Union[list[ApexUserRankDatabaseModel], None]:
-        """差分を含むランク情報を計算
-
-        Args:
-            ranks (list[dict]): ランク情報
-
-        Returns:
-            Union[list[ApexUserRankDatabaseModel], None]: 差分を含むランク情報
-        """
-        length = len(ranks)
-        if length == 0:
-            LogUtility.print_red('ユーザランク情報の取得に失敗しました。')
-            return None
-        elif length == 1:
-            return [ApexUserRankDatabaseModel(ranks[0])]
-        
-        histories: list[ApexUserRankDatabaseModel] = []
-        for rank in ranks:
-            histories.append(ApexUserRankDatabaseModel(rank))
-        for i in range(len(histories) - 1):
-            histories[i + 1].set_change(histories[i])
-        return histories
-
-    def calc_users_ranks_changes(self, users_ranks: list[list[dict]]) -> Union[list[list[ApexUserDatabaseModel]], None]:
-        """差分を含むランク情報を計算
-
-        Args:
-            users_ranks (list[list[dict]]): 複数のユーザのランク情報
-
-        Returns:
-            Union[list[list[ApexUserDatabaseModel]], None]: 差分を含むランク情報
-        """
-        users_histories = []
-        for user_ranks in users_ranks:
-            user_histories = self.calc_user_rank_changes(user_ranks)
-            if user_histories is not None:
-                users_histories.append(user_histories)
-        return users_histories
 
 def setup(bot: Client):
     return bot.add_cog(ApexStats(bot))
---
+++ utilities/apex_user_rank_utility.py
@@ -0,0 +1,187 @@
+import os
+from typing import Union
+from models.bot.apex_user_rank_model import ApexUserRankModel
+from models.database.apex_user_database_model import ApexUserDatabaseModel
+from models.database.apex_user_rank_database_model import ApexUserRankDatabaseModel
+from utilities.apis.apex_legends_status_api import ApexLegendsStatusAPI
+from utilities.database.database_apex_user import DatabaseApexUserUrility
+from utilities.database.database_apex_user_rank import DatabaseApexUserRankUrility
+from utilities.log import LogUtility
+if os.path.exists('pro.mode'):
+    import secret.secret_pro as secret
+    import secret.const_pro as const
+else:
+    import secret.secret_dev as secret
+    import secret.const_dev as const
+
+class ApexUserRankUtility():
+    def __init__(self):
+        self.__prev_update_user_ranks : list[ApexUserRankDatabaseModel] = None
+
+    async def get_apex_user(self, uid: int) -> ApexUserDatabaseModel:
+        """UIDからユーザ情報を取得
+
+        Args:
+            uid (int): ApexユーザUID
+
+        Raises:
+            Exception: 指定のユーザが見つからない
+
+        Returns:
+            list[ApexUserDatabaseModel]: ユーザ情報
+        """
+        users = self.get_registerd_users()
+        if users is None or len(users) == 0:
+            raise Exception('ユーザが登録されていません。先に[/apex_user add ~]を実行してください。')
+
+        regsted_uids = [user.uid for user in users]
+        is_registerd = (uid in regsted_uids)
+        if not is_registerd:
+            raise Exception(f'ユーザ({uid})が登録されていません。')
+        
+        user = [user for user in users if user.uid == uid][0]
+        return user
+
+    async def regist_apex_user(self, uid: int, name: str, platform: str) -> Union[ApexUserRankModel, None]:
+        """ユーザ情報をDBに登録
+
+        Args:
+            uid (int): uidまたはnameの指定が必須
+            name (str): uidまたはnameの指定が必須
+            platform (str): プラットフォーム(PC, PS4, X1, SWITCH)
+
+        Returns:
+            Union[ApexUserRankModel, None]: ランク情報を含むユーザ情報
+        """
+        if uid is None and name is None:
+            return None
+        
+        user = None
+        try:
+            if not uid is None:
+                user = await ApexLegendsStatusAPI.get_user_by_uid(uid, platform)
+            elif not name is None:
+                user = await ApexLegendsStatusAPI.get_user_by_name(name, platform)
+        except:
+            raise
+
+        if user is None:
+            return None
+        with DatabaseApexUserUrility() as database:
+            database.update_by_uid(user)
+            return user
+
+    def get_registerd_users(self) -> list[ApexUserDatabaseModel]:
+        """データベースに登録済みのユーザ情報を取得
+        """
+        with DatabaseApexUserUrility() as database:
+            users_list = database.select_users()
+            users = [ApexUserDatabaseModel(user) for user in users_list]
+            return users
+
+    def store_prev_users_rank(self, users_rank: list[ApexUserRankDatabaseModel]):
+        """前回のランク情報を保存
+
+        Args:
+            users_rank (list[ApexUserRankDatabaseModel]): 前回のユーザランク情報
+        """
+        self.__prev_update_user_ranks = users_rank
+
+    def get_changed_user_ranks(self, users_rank: list[ApexUserRankDatabaseModel]) -> list[ApexUserRankDatabaseModel]:
+        """前回と異なるランク情報のみを取得
+
+        Args:
+            users_rank (list[ApexUserRankDatabaseModel]): 今回のユーザランク情報
+
+        Returns:
+            list[ApexUserRankDatabaseModel]: 更新のあったランク情報
+        """
+        if self.__prev_update_user_ranks is None:
+            self.store_prev_users_rank(users_rank)
+            return users_rank
+        
+        changed_users_rank = [user for user in users_rank if not user in self.__prev_update_user_ranks]
+        self.store_prev_users_rank(users_rank)
+
+        return changed_users_rank
+
+    async def refresh_apex_user_rank(self, uid: int) -> Union[ApexUserRankModel, ApexUserRankDatabaseModel, None]:
+        """データベースに登録済みの追跡対象ユーザのランク情報を取得して登録
+        """
+        user = None
+        with DatabaseApexUserUrility() as database:
+            user = database.select_by_uid(uid=uid)
+
+        rank_histories = None
+        refreshed_user = await ApexLegendsStatusAPI.get_user_by_uid(user.uid, user.platform)
+        with DatabaseApexUserRankUrility() as database:
+            database.insert_rank_by_uid(refreshed_user)
+            rank_histories = database.select_by_user_uid(user.uid, 2)
+
+        if rank_histories is None:
+            return refreshed_user
+        ranks = self.calc_user_rank_changes(rank_histories)
+        if ranks is None:
+            return None
+        return ranks[-1]
+
+    async def refresh_apex_users_rank(self) -> Union[list[ApexUserRankModel], list[ApexUserRankDatabaseModel], None]:
+        """データベースに登録されている追跡対象のユーザすべてのランク情報を取得して登録
+        """
+        users = self.get_registerd_users()
+        if users is None or len(users) == 0:
+            return None
+
+        rank_histories = None
+        refreshed_users = [await ApexLegendsStatusAPI.get_user_by_uid(uid=user.uid, platform=user.platform) for user in users]
+        with DatabaseApexUserRankUrility() as database:
+            database.insert_ranks_by_uid(refreshed_users)
+            uids = [user.uid for user in users]
+            rank_histories = database.select_by_users_uid(uids, 2)
+
+        if rank_histories is None:
+            return refreshed_users
+        
+        # 最新のランク情報のみ抽出
+        users_ranks = self.calc_users_ranks_changes(rank_histories)
+        users_rank = [users_rank[-1] for users_rank in users_ranks]
+        return users_rank
+
+    def calc_user_rank_changes(self, ranks: list[dict]) -> Union[list[ApexUserRankDatabaseModel], None]:
+        """差分を含むランク情報を計算
+
+        Args:
+            ranks (list[dict]): ランク情報
+
+        Returns:
+            Union[list[ApexUserRankDatabaseModel], None]: 差分を含むランク情報
+        """
+        length = len(ranks)
+        if length == 0:
+            LogUtility.print_red('ユーザランク情報の取得に失敗しました。')
+            return None
+        elif length == 1:
+            return [ApexUserRankDatabaseModel(ranks[0])]
+        
+        histories: list[ApexUserRankDatabaseModel] = []
+        for rank in ranks:
+            histories.append(ApexUserRankDatabaseModel(rank))
+        for i in range(len(histories) - 1):
+            histories[i + 1].set_change(histories[i])
+        return histories
+
+    def calc_users_ranks_changes(self, users_ranks: list[list[dict]]) -> Union[list[list[ApexUserDatabaseModel]], None]:
+        """差分を含むランク情報を計算
+
+        Args:
+            users_ranks (list[list[dict]]): 複数のユーザのランク情報
+
+        Returns:
+            Union[list[list[ApexUserDatabaseModel]], None]: 差分を含むランク情報
+        """
+        users_histories = []
+        for user_ranks in users_ranks:
+            user_histories = self.calc_user_rank_changes(user_ranks)
+            if user_histories is not None:
+                users_histories.append(user_histories)
+        return users_histories
